764464c841342a77fca9365ad55c4ae218e8bcd1
Brent Baskin <55645256+onshortly@users.noreply.github.com>
2024-08-06 19:12:46 +0000

    add living room

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Main } from "./pages/Main";
 import Jaunt from "./pages/Jaunt";
 import ScrollToTop from "./components/ScrollToTop";
+import { LivingRoom } from "./pages/LivingRoom";
 
 function App() {
 
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" Component={Main} />
         <Route path="/jaunt" Component={Jaunt} />
         <Route path="/about" element={<p>about me</p>} />
+        <Route path="/living-room" Component={LivingRoom} />
       </Routes>
       {/* <div style={{ position: 'fixed', bottom: 20, right: 20 }}>
                     <Achievement />
---
+++ src/pages/LivingRoom.tsx
@@ -0,0 +1,5 @@
+export const LivingRoom = () => {
+    return (
+        <iframe title="livingRoom" src="https://itch.io/embed/2881741" width="552" height="167" frameBorder="0"><a href="https://ons-hortly.itch.io/llliving-room">Llliving Room by ons_hortly</a></iframe>
+    )
+}
